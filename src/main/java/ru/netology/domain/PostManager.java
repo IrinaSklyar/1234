c041ead709a289ba5226420f26a44fcc8542d3a2
Irina Skliarova <i.skliarova2015@yandex.ru>
2020-05-24 20:10:55 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/PostManager.java
@@ -0,0 +1,25 @@
+package ru.netology.domain;
+
+public class PostManager {
+    private int id;
+    private String contactInformation;
+    private String imageUrl;
+    private String text;
+    private int data;
+    private boolean like;
+    private int view;
+    private int repost;
+    private boolean postIsPinned;
+    private boolean canResponse;
+    private boolean canDelete;
+    private boolean canEdit;
+
+    public Post[] search (){
+
+        return null;
+    }
+    public void delete () {
+
+    }
+}
+
