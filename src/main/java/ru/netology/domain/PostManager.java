a225be8c2c8a6e8923a4d5f1e33c4fa593da594b
Irina Skliarova <i.skliarova2015@yandex.ru>
2020-06-16 09:43:42 +0300

    Fix #1

--- src/main/java/ru/netology/domain/PostManager.java
+++ src/main/java/ru/netology/domain/PostManager.java
@@ -1,36 +1,18 @@
 package ru.netology.domain;
 
 public class PostManager {
-    int owner_id;
-    String domain;
-    String query;
-    String item;
-    boolean owners_only;
-    int count;
-
 
     private Post[] post = new Post[0];
 
-    public Post[] search() {
-        // принимает на вход следующие параметры:
-        int owner_id;
-        String[] items;
-        String query;
-        // перебор элементов массива с помощью функции foreach
-        // сравнить со значением "String query";
-        // вывод количества совпадующих элементов (int count);
-    }
-    // возврат массива с совпадающими значениями;
-        return null;
-}
+    public Post[] search(int ownerId, String query, String[] items) {
 
-    public void delete {
-        // принимает на вход следующие параметры:
-        int owner_id
-        int post_id
-        boolean owners_only;
-        // перебираент массив (foreach)
-        // удаляет запись (заданный номер), если условие соблюдено "boolean true"
-        }
+        return null;
+    }
+
+    public Post[] delete (int ownerId, int postId,  boolean owners_only) {
+
+        return null;
+    }
+}
 
 
